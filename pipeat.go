856b1493cbc546e1c9cb45d25f2f7dfcf6d4a251
John Eikenberry <jae@zhar.net>
2016-08-29 12:04:55 -0700

    initial working version

---
+++ pipeat.go
@@ -0,0 +1,155 @@
+package pipeat
+
+// Attempts to provide similar funcationality to io.Pipe except supporting
+// ReaderAt and WriterAt. It uses a temp file as a buffer in order to implement
+// the interfaces as well as allow for asyncronous writes.
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"sync"
+)
+
+// Used to track write ahead areas of a file. That is, areas where there is a
+// gap in the file data earlier in the file. Possible with concurrent writes.
+type span struct {
+	start, end int64
+}
+
+type spans []span
+
+func (c spans) Len() int           { return len(c) }
+func (c spans) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
+func (c spans) Less(i, j int) bool { return c[i].start < c[j].start }
+
+// The wrapper around the underlying temp file.
+type pipeFile struct {
+	*os.File
+	sync.Mutex // serialize access to meta data (below)
+	sync.Cond  // used with Mutex to signal readers from writers
+	endln      int64
+	ahead      spans
+	eof        chan struct{} // closed when all writes complete
+}
+
+func newPipeFile() (*pipeFile, error) {
+	file, err := ioutil.TempFile("", "test")
+	if err != nil {
+		return nil, err
+	}
+	os.Remove(file.Name())
+	b := &pipeFile{File: file, eof: make(chan struct{})}
+	b.L = b // Cond locker
+	return b, nil
+}
+
+func (b *pipeFile) readable(start, end int64) bool {
+	return (start+end < b.endln)
+}
+
+// io.WriterAt side of pipe.
+type PipeWriterAt struct {
+	b *pipeFile
+}
+
+// io.ReaderAt side of pipe.
+type PipeReaderAt struct {
+	b *pipeFile
+}
+
+// Pipe creates an asynchronous file based pipe. It can be used to connect code
+// expecting an io.ReaderAt with code expecting an io.WriterAt. Writes all go
+// to an unlinked temporary file, reads start up as the file gets written up to
+// their area. It is safe to call multiple ReadAt and WriteAt in parallel with
+// each other. Due to the asyncronous nature, Close needs to be called on the
+// writer first, then the reader. If you call Close on the reader first it will
+// block until the Writer's Close is called.
+func Pipe() (*PipeReaderAt, *PipeWriterAt, error) {
+	fp, err := newPipeFile()
+	if err != nil {
+		return nil, nil, err
+	}
+	return &PipeReaderAt{fp}, &PipeWriterAt{fp}, nil
+}
+
+// ReadAt implements the standard ReaderAt interface. It blocks if it gets
+// ahead of the writer. You can call it from multiple threads.
+func (r *PipeReaderAt) ReadAt(p []byte, off int64) (int, error) {
+	trace("readat", off)
+	defer trace("successfully read:", off, string(p))
+	r.b.Lock()
+	for {
+		if !r.b.readable(off, int64(len(p))) {
+			select {
+			case <-r.b.eof:
+				trace("EOF")
+			default:
+				r.b.Wait()
+				continue
+			}
+		}
+		r.b.Unlock()
+		return r.b.File.ReadAt(p, off)
+	}
+}
+
+// Close will block until writer closes, then it will Close the temp file.
+func (r *PipeReaderAt) Close() error {
+	select {
+	case <-r.b.eof:
+		r.b.Lock()
+		defer r.b.Unlock()
+		return r.b.File.Close()
+	}
+}
+
+// WriteAt implements the standard WriterAt interface. It will write to the
+// temp file without blocking. You can call it from multiple threads.
+func (w *PipeWriterAt) WriteAt(p []byte, off int64) (int, error) {
+	trace("writeat: ", string(p), off)
+	defer trace("wrote: ", string(p), off)
+	n, err := w.b.File.WriteAt(p, off)
+	w.b.Lock()
+	defer w.b.Unlock()
+	if off == w.b.endln {
+		w.b.endln = w.b.endln + int64(n)
+		newtip := 0
+		for i, s := range w.b.ahead {
+			if s.start == w.b.endln {
+				w.b.endln = s.end
+				newtip = i + 1
+			}
+		}
+		if newtip > 0 { // clean up ahead queue
+			w.b.ahead = append(w.b.ahead[:0], w.b.ahead[newtip:]...)
+		}
+		w.b.Broadcast()
+	} else {
+		// XXX should be limit on ahead's length
+		w.b.ahead = append(w.b.ahead, span{off, off + int64(n)})
+		sort.Sort(w.b.ahead)
+	}
+	// trace(w.b.ahead)
+	return n, err
+}
+
+// Close on the writer will let the reader know that writing is complete. Once
+// the reader catches up it will continue to return 0 bytes and an EOF error.
+func (w *PipeWriterAt) Close() error {
+	w.b.Lock()
+	defer w.b.Unlock()
+	close(w.b.eof)
+	w.b.Broadcast()
+	return nil
+}
+
+// debugging stuff
+const watch = false
+
+func trace(p ...interface{}) {
+	if watch {
+		fmt.Println(p...)
+	}
+}
---
+++ pipeat_test.go
@@ -0,0 +1,298 @@
+package pipeat
+
+import (
+	"bytes"
+	"io"
+	"math/rand"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var paragraph = "In programming, concurrency is the composition of independently executing processes, while parallelism is the simultaneous execution of (possibly related) computations. Concurrency is about dealing with lots of things at once. Parallelism is about doing lots of things at once."
+
+type chunk struct {
+	offset int64
+	word   []byte
+}
+
+func chunkify(s string) []chunk {
+	ss := strings.Split(s, " ")
+	chunks := make([]chunk, len(ss))
+	offset := 0
+	for i, s := range ss {
+		if i+1 != len(ss) {
+			s = s + " "
+		}
+		chunks[i] = chunk{int64(offset), []byte(s)}
+		offset += len(s)
+	}
+	return chunks
+}
+
+func segmentify(chunks []chunk, num int) [][]chunk {
+	segments := make([][]chunk, num)
+	seg_size := (len(chunks) / num) + 1
+	var start, end int
+	for i := 0; i < num; i++ {
+		start, end = end, end+seg_size
+		if end <= len(chunks) {
+			segments[i] = chunks[start:end]
+		} else {
+			segments[i] = chunks[start:]
+		}
+	}
+	return segments
+}
+
+func reverse(t []chunk) []chunk {
+	for l, r := 0, len(t)-1; l < r; l, r = l+1, r-1 {
+		t[l], t[r] = t[r], t[l]
+	}
+	return t
+}
+
+func slowdown(i int) time.Duration {
+	return time.Duration(i * 13)
+}
+
+type reader struct {
+	r           io.ReaderAt
+	wg          *sync.WaitGroup
+	delay       int
+	dest        chan chunk
+	buf         *bytes.Buffer
+	*sync.Mutex // to protect buf
+}
+
+func newReader(r io.ReaderAt, wg *sync.WaitGroup, delay int) *reader {
+	return &reader{
+		r: r, wg: wg, delay: delay,
+		dest:  make(chan chunk, 1000), // big enough to hold all words
+		Mutex: &sync.Mutex{},
+	}
+}
+
+func sectionReader(r *reader, wg *sync.WaitGroup, start, end int64) *reader {
+	section := io.NewSectionReader(r.r, start, end)
+	return newReader(section, wg, r.delay)
+}
+
+func (r *reader) buffer() {
+	r.Lock()
+	defer r.Unlock()
+	if r.buf != nil {
+		return
+	}
+	var size int
+	parts := []chunk{}
+	close(r.dest)
+	for t := range r.dest {
+		parts = append(parts, t)
+		size += len(t.word)
+	}
+	trace("parts length:", len(parts))
+	result := make([]byte, size)
+	for _, t := range parts {
+		trace("--", t.offset, len(t.word), string(t.word))
+		start, end := t.offset, len(t.word)+int(t.offset)
+		copy(result[start:end], t.word)
+	}
+	r.buf = bytes.NewBuffer(result)
+}
+
+func (r *reader) String() string {
+	r.buffer()
+	return r.buf.String()
+}
+
+func simpleReader(t *testing.T, r *reader) {
+	defer r.wg.Done()
+	offset := 0
+	size := 10
+	for {
+		b := make([]byte, size)
+		n, err := r.r.ReadAt(b, int64(offset))
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+		r.dest <- chunk{offset: int64(offset), word: b[:n]}
+		if n == 0 || err == io.EOF {
+			return
+		}
+		offset += n
+		time.Sleep(time.Millisecond * time.Duration(r.delay))
+	}
+}
+
+func ccReader(t *testing.T, r *reader, workers int) {
+	seg_size := int64((len(paragraph) / workers) + 1)
+	var sections = make([]*reader, workers)
+	var start, end int64
+	results_wg := &sync.WaitGroup{}
+	results_wg.Add(workers)
+	for i := 0; i < workers; i++ {
+		start, end = end, end+seg_size
+		sections[i] = sectionReader(r, results_wg, start, seg_size)
+	}
+	for i := 0; i < workers; i++ {
+		go simpleReader(t, sections[i])
+	}
+	results_wg.Wait()
+	r.buf = bytes.NewBuffer(nil)
+	for i := 0; i < workers; i++ {
+		sections[i].buffer()
+		r.buf.Write(sections[i].buf.Bytes())
+		r.wg.Done()
+	}
+}
+
+func simpleWriter(t *testing.T, w *PipeWriterAt, delay int) {
+	for _, t := range chunkify(paragraph) {
+		time.Sleep(time.Millisecond * time.Duration(delay))
+		w.WriteAt(t.word, t.offset)
+	}
+	w.Close()
+}
+
+func reverseWriter(t *testing.T, w *PipeWriterAt, delay int) {
+	chunks := reverse(chunkify(paragraph))
+	for _, t := range chunks {
+		time.Sleep(time.Millisecond * time.Duration(delay))
+		w.WriteAt(t.word, t.offset)
+	}
+	w.Close()
+}
+
+func randomWriter(t *testing.T, w *PipeWriterAt, delay int) {
+	chunks := chunkify(paragraph)
+	picks := rand.Perm(len(chunks))
+	for _, i := range picks {
+		t := chunks[i]
+		time.Sleep(time.Millisecond * time.Duration(delay))
+		w.WriteAt(t.word, t.offset)
+	}
+	w.Close()
+}
+
+func ccWriter(t *testing.T, w *PipeWriterAt, delay, workers int) {
+	chunks := chunkify(paragraph)
+	segments := segmentify(chunks, workers)
+	results_wg := &sync.WaitGroup{}
+	results_wg.Add(workers)
+	for i := 0; i < len(segments); i++ {
+		go func(j int, segment []chunk) {
+			for _, t := range segment {
+				time.Sleep(time.Millisecond * time.Duration(delay))
+				w.WriteAt(t.word, t.offset)
+			}
+			results_wg.Done()
+		}(i, segments[i])
+	}
+	results_wg.Wait()
+	w.Close()
+}
+
+func TestCcWrite(t *testing.T) {
+	r, w, err := Pipe()
+	if err != nil {
+		panic(err)
+	}
+	workers := 4
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	reader := newReader(r, wg, 0)
+	go simpleReader(t, reader)
+	go ccWriter(t, w, 0, workers)
+	wg.Wait()
+	r.Close()
+	assert.Equal(t, reader.String(), paragraph)
+}
+
+func TestCcRead(t *testing.T) {
+	r, w, err := Pipe()
+	if err != nil {
+		panic(err)
+	}
+	workers := 4
+
+	wg := &sync.WaitGroup{}
+	wg.Add(workers)
+	reader := newReader(r, wg, 0)
+	go ccReader(t, reader, workers)
+	go simpleWriter(t, w, 1)
+	wg.Wait()
+	r.Close()
+	assert.Equal(t, reader.String(), paragraph)
+}
+
+func TestCcRWrite(t *testing.T) {
+	r, w, err := Pipe()
+	if err != nil {
+		panic(err)
+	}
+	workers := 4
+
+	wg := &sync.WaitGroup{}
+	wg.Add(workers)
+	reader := newReader(r, wg, 0)
+	go ccReader(t, reader, workers)
+	go ccWriter(t, w, 0, workers)
+	wg.Wait()
+	r.Close()
+	assert.Equal(t, reader.String(), paragraph)
+}
+
+func TestSimpleWrite(t *testing.T) {
+	r, w, err := Pipe()
+	if err != nil {
+		panic(err)
+	}
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	reader := newReader(r, wg, 0)
+	go simpleReader(t, reader)
+	// Writer
+	go simpleWriter(t, w, 1)
+	wg.Wait()
+	r.Close()
+	trace(reader.String())
+	assert.Equal(t, reader.String(), paragraph)
+}
+
+func TestReverseWrite(t *testing.T) {
+	r, w, err := Pipe()
+	if err != nil {
+		panic(err)
+	}
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	reader := newReader(r, wg, 0)
+	go simpleReader(t, reader)
+	go reverseWriter(t, w, 1)
+	wg.Wait()
+	r.Close()
+	trace(reader.String())
+	assert.Equal(t, reader.String(), paragraph)
+}
+
+func TestRandomWrite(t *testing.T) {
+	rand.Seed(17)
+	r, w, err := Pipe()
+	if err != nil {
+		panic(err)
+	}
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	reader := newReader(r, wg, 0)
+	go simpleReader(t, reader)
+	go randomWriter(t, w, 1)
+	wg.Wait()
+	r.Close()
+	trace(reader.String(), len(reader.String()))
+	assert.Equal(t, reader.String(), paragraph)
+}
